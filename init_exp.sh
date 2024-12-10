69a1ad7631938cad16184c228b590147fda1f68b
jiangbojian <475542598@qq.com>
2024-12-10 15:40:54 +0800

    Update init_exp.sh

--- init_exp.sh
+++ init_exp.sh
@@ -1,17 +1,16 @@
 set -ex
 
+mkdir checkpoints record
 
-cd `pwd`
-rm -rf checkpoints/*
-
-cp -r ../env/DeepSpeedExamples-master .
-# manually downlaod https://huggingface.co/openbmb/UltraRM-13b ,  and cp to ../env/DeepSpeedExamples-master/applications/DeepSpeed-Chat/openbmb/UltraRM-13b/UltraRM-13b
+cp -r env/DeepSpeedExamples-master .
+# manually downlaod https://huggingface.co/openbmb/UltraRM-13b 
+cp -r UltraRM-13b DeepSpeedExamples-master/applications/DeepSpeed-Chat/openbmb/UltraRM-13b/UltraRM-13b
 
 mkdir record/epoch-0/sft/red/step1 -p
-cp ../env/data/sft/init/red.v2/step1/stage2_red.train record/epoch-0/sft/red/step1/red.trainset
+cp env/data/sft/init/red.v2/step1/stage2_red.train record/epoch-0/sft/red/step1/red.trainset
 
 ## init history attack prompt
-cp ../env/data/attack/create_data/prompt record/epoch-0/prompt_database
-cp ../env/data/attack/create_data/prompt record/epoch-0/history_prompt
+cp env/data/attack/create_data/prompt record/epoch-0/prompt_database
+cp env/data/attack/create_data/prompt record/epoch-0/history_prompt
 
-cp -r ../env/data .
+cp -r env/data .
